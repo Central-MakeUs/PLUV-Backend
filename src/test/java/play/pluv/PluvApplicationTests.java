2d9121edb300536aded260c32328369afa7617bd
hong-sile <hyukjoon.dev@gmail.com>
2024-07-08 23:55:34 +0900

    init project

---
+++ src/test/java/play/pluv/PluvApplicationTests.java
@@ -0,0 +1,13 @@
+package play.pluv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluvApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

00112ba6786df4d1089cd1b8559b811f0fef9047
hong-sile <hyukjoon.dev@gmail.com>
2024-07-10 15:46:17 +0900

    feat: 불필요한 옵션 제거(#9)

--- src/main/java/play/pluv/PluvApplication.java
+++ src/main/java/play/pluv/PluvApplication.java
@@ -2,9 +2,7 @@ package play.pluv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 public class PluvApplication {
 

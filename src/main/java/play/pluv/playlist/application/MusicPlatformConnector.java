4bc27d76af0c0355402f594e08dc7272a61af8a6
hong-sile <hyukjoon.dev@gmail.com>
2024-07-15 13:15:27 +0900

    fix: List를 반환하도록 수정(#15)

--- src/main/java/play/pluv/playlist/application/MusicPlatformConnector.java
+++ src/main/java/play/pluv/playlist/application/MusicPlatformConnector.java
@@ -1,11 +1,13 @@
 package play.pluv.playlist.application;
 
+import java.util.List;
 import play.pluv.playlist.domain.PlayList;
 import play.pluv.playlist.domain.PlayListProvider;
 
 public interface MusicPlatformConnector {
 
-  PlayList getPlayList(final String authCode);
+  //TODO : 최신순 정렬 -> 불가능
+  List<PlayList> getPlayList(final String authCode);
 
   PlayListProvider supportedType();
 }

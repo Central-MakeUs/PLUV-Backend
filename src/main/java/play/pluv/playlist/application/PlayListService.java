451081e4dfaf953c0c70c09f496753e513e47ebb
hong-sile <hyukjoon.dev@gmail.com>
2024-07-15 11:43:45 +0900

    test: PlayList 읽는 기능 추가(#15)

--- src/main/java/play/pluv/playlist/application/PlayListService.java
+++ src/main/java/play/pluv/playlist/application/PlayListService.java
@@ -1,19 +1,17 @@
 package play.pluv.playlist.application;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import play.pluv.playlist.domain.PlayList;
 
 @Service
+@RequiredArgsConstructor
 public class PlayListService {
 
   private final MusicPlatformConnector musicPlatformConnector;
 
-  public PlayListService(final MusicPlatformConnector musicPlatformConnector) {
-    this.musicPlatformConnector = musicPlatformConnector;
-  }
-
   public List<PlayList> getPlayLists(final String authCode, final String source) {
-    return List.of();
+    return musicPlatformConnector.getPlayList(authCode);
   }
 }
--- src/test/java/play/pluv/playlist/application/PlayListServiceTest.java
+++ src/test/java/play/pluv/playlist/application/PlayListServiceTest.java
@@ -1,8 +1,9 @@
-package play.pluv.oauth.spotify;
+package play.pluv.playlist.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static play.pluv.fixture.PlayListFixture.스포티파이_플레이리스트_1;
 import static play.pluv.fixture.PlayListFixture.스포티파이_플레이리스트_2;
+import static play.pluv.playlist.domain.PlayListProvider.SPOTIFY;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -10,16 +11,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import play.pluv.playlist.domain.PlayList;
 import play.pluv.support.ApplicationTest;
 
-class SpotifyConnectorTest extends ApplicationTest {
+class PlayListServiceTest extends ApplicationTest {
 
   @Autowired
-  private SpotifyConnector spotifyConnector;
+  private PlayListService playListService;
 
   @Test
-  void 정상적으로_PlayList_객체를_반환한다() {
+  void 플레이리스트를_읽는다() {
     final List<PlayList> expected = List.of(스포티파이_플레이리스트_1(), 스포티파이_플레이리스트_2());
 
-    final List<PlayList> actual = spotifyConnector.getPlayList("authCode");
+    final List<PlayList> actual = playListService.getPlayLists("authCode", SPOTIFY.getName());
 
     assertThat(actual)
         .usingRecursiveFieldByFieldElementComparator()

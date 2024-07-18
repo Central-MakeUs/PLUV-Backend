ba7e408e75935e9bf45ba59faf863cf002f68edb
hong-sile <hyukjoon.dev@gmail.com>
2024-07-18 12:51:20 +0900

    fix: Config가 정상적으로 적용되지 않는 오류 수정(#7)

--- src/test/java/play/pluv/support/ApplicationTest.java
+++ src/test/java/play/pluv/support/ApplicationTest.java
@@ -8,18 +8,21 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import play.pluv.config.DatabaseCleanerExtension;
 import play.pluv.fixture.SpotifyFixture;
 import play.pluv.oauth.spotify.SpotifyApiClient;
+import play.pluv.support.ApplicationTest.MockBeanConfig;
 
 @SpringBootTest
 @DisplayNameGeneration(ReplaceUnderscores.class)
 @ExtendWith(DatabaseCleanerExtension.class)
+@Import(MockBeanConfig.class)
 public abstract class ApplicationTest {
 
   @Configuration
-  public static class mockBeanConfig {
+  public static class MockBeanConfig {
 
     @Bean
     @Primary

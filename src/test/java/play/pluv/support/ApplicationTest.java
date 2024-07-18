4d2bae0d4c54edc4748d0c72261e3fd283895a47
hong-sile <hyukjoon.dev@gmail.com>
2024-07-18 12:47:51 +0900

    test: 테스트 환경에 Extension 추가

--- src/test/java/play/pluv/support/ApplicationTest.java
+++ src/test/java/play/pluv/support/ApplicationTest.java
@@ -1,20 +1,21 @@
 package play.pluv.support;
 
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import play.pluv.config.DatabaseCleanerExtension;
 import play.pluv.fixture.SpotifyFixture;
 import play.pluv.oauth.spotify.SpotifyApiClient;
 
 @SpringBootTest
 @DisplayNameGeneration(ReplaceUnderscores.class)
+@ExtendWith(DatabaseCleanerExtension.class)
 public abstract class ApplicationTest {
 
   @Configuration

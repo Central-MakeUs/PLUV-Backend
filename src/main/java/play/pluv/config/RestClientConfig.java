d6ea1e806822f5f1d656334821e7842957e31a15
hong-sile <hyukjoon.dev@gmail.com>
2024-07-19 21:16:18 +0900

    feat: 다른 환경변수 값으로 log 활성화 여부 분리

--- src/main/java/play/pluv/config/RestClientConfig.java
+++ src/main/java/play/pluv/config/RestClientConfig.java
@@ -1,10 +1,12 @@
 package play.pluv.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestClient.Builder;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 import play.pluv.oauth.spotify.SpotifyApiClient;
@@ -13,9 +15,14 @@ import play.pluv.oauth.spotify.SpotifyApiClient;
 public class RestClientConfig {
 
   private final LoggingInterceptor loggingInterceptor;
+  private final Boolean logActive;
 
-  public RestClientConfig(final LoggingInterceptor loggingInterceptor) {
+  public RestClientConfig(
+      final LoggingInterceptor loggingInterceptor,
+      @Value("${restClient.logging}") final boolean logActive
+  ) {
     this.loggingInterceptor = loggingInterceptor;
+    this.logActive = logActive;
   }
 
   @Bean
@@ -24,14 +31,24 @@ public class RestClientConfig {
   }
 
   private <T> T createHttpInterface(final Class<T> clazz) {
-    final RestClient restClient = RestClient.builder()
-        .requestInterceptor(loggingInterceptor)
-        .requestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
-        .build();
+    Builder builder = RestClient.builder();
+    if (logActive) {
+      builder = addLogInterceptor(builder);
+    }
+    final RestClient restClient = builder.build();
     final RestClientAdapter adapter = RestClientAdapter.create(restClient);
     final HttpServiceProxyFactory factory = HttpServiceProxyFactory
         .builderFor(adapter)
         .build();
     return factory.createClient(clazz);
+  }
+
+  private Builder addLogInterceptor(Builder builder) {
+    builder = builder
+        .requestInterceptor(loggingInterceptor)
+        .requestFactory(
+            new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory())
+        );
+    return builder;
   }
 }

044e7225bd399de8876ba7f422768460f74f5776
hong-sile <hyukjoon.dev@gmail.com>
2024-07-19 21:02:56 +0900

    feat: 로그 인터셉터 RestClient에 추가

--- src/main/java/play/pluv/config/RestClientConfig.java
+++ src/main/java/play/pluv/config/RestClientConfig.java
@@ -2,6 +2,8 @@ package play.pluv.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -10,13 +12,22 @@ import play.pluv.oauth.spotify.SpotifyApiClient;
 @Configuration
 public class RestClientConfig {
 
+  private final LoggingInterceptor loggingInterceptor;
+
+  public RestClientConfig(final LoggingInterceptor loggingInterceptor) {
+    this.loggingInterceptor = loggingInterceptor;
+  }
+
   @Bean
   public SpotifyApiClient spotifyApiClient() {
     return createHttpInterface(SpotifyApiClient.class);
   }
 
   private <T> T createHttpInterface(final Class<T> clazz) {
-    final RestClient restClient = RestClient.create();
+    final RestClient restClient = RestClient.builder()
+        .requestInterceptor(loggingInterceptor)
+        .requestFactory(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+        .build();
     final RestClientAdapter adapter = RestClientAdapter.create(restClient);
     final HttpServiceProxyFactory factory = HttpServiceProxyFactory
         .builderFor(adapter)

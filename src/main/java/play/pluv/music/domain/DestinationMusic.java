c11bba309157b91ef566ded3a2ca467e60b1e41c
hong-sile <hyukjoon.dev@gmail.com>
2024-07-20 22:15:56 +0900

    feat: 동일성을 비교하는 로직 추가(#19)

--- src/main/java/play/pluv/music/domain/DestinationMusic.java
+++ src/main/java/play/pluv/music/domain/DestinationMusic.java
@@ -1,6 +1,9 @@
 package play.pluv.music.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,10 +17,22 @@ public class DestinationMusic {
   private final List<String> artistNames;
   private final String name;
   private final String imageUrl;
+  //nullable한 값
   private final String isrcCode;
 
   public Boolean isSame(final SourceMusic sourceMusic) {
+    if (isrcCode != null) {
+      return sourceMusic.getIsrcCode()
+          .map(isrc -> isrc.equals(isrcCode))
+          .orElseGet(() -> compareName(sourceMusic));
+    }
+    return compareName(sourceMusic);
+  }
 
-    return null;
+  private Boolean compareName(final SourceMusic sourceMusic) {
+    final Set<String> set1 = new HashSet<>(artistNames);
+    final Set<String> set2 = new HashSet<>(sourceMusic.getArtistNames());
+
+    return Objects.equals(sourceMusic.getName(), name) && set1.equals(set2);
   }
 }

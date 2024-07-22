cc9be59834844efd11daca7913221c372b7a02db
hong-sile <hyukjoon.dev@gmail.com>
2024-07-22 10:21:21 +0900

    feat: 스포티파이 플레이리스트의 음악을 읽는 기능 추가(#23)

--- src/main/java/play/pluv/playlist/controller/PlayListController.java
+++ src/main/java/play/pluv/playlist/controller/PlayListController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import play.pluv.base.BaseResponse;
 import play.pluv.playlist.application.PlayListService;
+import play.pluv.playlist.application.dto.PlayListMusicResponse;
 import play.pluv.playlist.application.dto.PlayListOverViewResponse;
 import play.pluv.playlist.application.dto.PlayListReadRequest;
 
@@ -26,5 +28,15 @@ public class PlayListController {
     final var playLists = playListService.getPlayLists(request.accessToken(), source);
     final List<PlayListOverViewResponse> response = PlayListOverViewResponse.createList(playLists);
     return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/{source}/playLists/{id}/read")
+  public BaseResponse<List<PlayListMusicResponse>> readPlayLists(
+      @PathVariable final String source, @RequestBody final PlayListReadRequest request,
+      @PathVariable final String id
+  ) {
+    final var musics = playListService.getPlayListMusics(request.accessToken(), id);
+    final List<PlayListMusicResponse> response = PlayListMusicResponse.createList(musics);
+    return BaseResponse.ok(response);
   }
 }

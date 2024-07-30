ffaea93671dda82c5149831c961242aa47987743
hong-sile <hyukjoon.dev@gmail.com>
2024-07-30 09:22:37 +0900

    feat: google social 로그인 추가(#40)

--- src/main/java/play/pluv/oauth/google/GoogleApiClient.java
+++ src/main/java/play/pluv/oauth/google/GoogleApiClient.java
@@ -1,5 +1,11 @@
 package play.pluv.oauth.google;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import play.pluv.oauth.google.dto.GoogleIdTokenResponse;
+
 public interface GoogleApiClient {
 
+  @GetMapping("https://oauth2.googleapis.com/tokeninfo")
+  GoogleIdTokenResponse verifyIdToken(@RequestParam("code") final String id_token);
 }
---
+++ src/main/java/play/pluv/oauth/google/dto/GoogleIdTokenResponse.java
@@ -0,0 +1,12 @@
+package play.pluv.oauth.google.dto;
+
+import static play.pluv.music.domain.MusicStreaming.YOUTUBE;
+
+import play.pluv.oauth.domain.OAuthMemberInfo;
+
+public record GoogleIdTokenResponse(String sub) {
+
+  public OAuthMemberInfo toOAuthMemberInfo() {
+    return new OAuthMemberInfo(sub, YOUTUBE);
+  }
+}

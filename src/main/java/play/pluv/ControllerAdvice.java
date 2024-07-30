0db422aa209abdfae60273d004382cc6a0a142e4
hong-sile <hyukjoon.dev@gmail.com>
2024-07-30 11:37:36 +0900

    feat: controllerAdvice 수정(#6)

--- src/main/java/play/pluv/ControllerAdvice.java
+++ src/main/java/play/pluv/ControllerAdvice.java
@@ -1,5 +1,7 @@
 package play.pluv;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 import java.net.BindException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,24 +19,26 @@ import play.pluv.base.BaseResponse;
 public class ControllerAdvice {
 
   @ExceptionHandler(BaseException.class)
-  public BaseResponse<String> handleCustomException(final BaseException exception) {
+  public ResponseEntity<BaseResponse<String>> handleCustomException(final BaseException exception) {
     final BaseExceptionType type = exception.getExceptionType();
     loggingClientException(exception, type.getMessage());
-    return BaseResponse.exception(type);
+    return new ResponseEntity<>(BaseResponse.exception(type), type.getHttpStatus());
   }
 
   @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentNotValidException.class})
-  public BaseResponse<String> handleHttpMessageNotReadableException(final Exception e) {
+  public ResponseEntity<BaseResponse<String>> handleHttpMessageNotReadableException(
+      final Exception e
+  ) {
     final String message = "입력 형식이 올바르지 않습니다.";
     loggingClientException(e, message);
-    return BaseResponse.badRequest(message);
+    return new ResponseEntity<>(BaseResponse.badRequest(message), BAD_REQUEST);
   }
 
   @ExceptionHandler(BindException.class)
-  public BaseResponse<String> handleBindExceptionHandler(final Exception e) {
+  public ResponseEntity<BaseResponse<String>> handleBindExceptionHandler(final Exception e) {
     final String message = "요청 파라미터가 올바르지 않습니다.";
     loggingClientException(e, message);
-    return BaseResponse.badRequest(message);
+    return new ResponseEntity<>(BaseResponse.badRequest(message), BAD_REQUEST);
   }
 
   @ExceptionHandler(NoResourceFoundException.class)
@@ -48,8 +52,10 @@ public class ControllerAdvice {
   }
 
   @ExceptionHandler(Exception.class)
-  public BaseResponse<String> handleUnExpectedException(final Exception exception) {
+  public ResponseEntity<BaseResponse<String>> handleUnExpectedException(final Exception exception) {
     log.error("[ERROR] MESSAGE : ", exception);
-    return BaseResponse.serverError("잠시 후 다시 시도해주시고 동일한 에러가 반복되는 경우 문의 부탁드립니다.");
+    final var response = BaseResponse.serverError(
+        "잠시 후 다시 시도해주시고 동일한 에러가 반복되는 경우 문의 부탁드립니다.");
+    return ResponseEntity.internalServerError().body(response);
   }
 }

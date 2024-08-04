3b8d06c59b0084968e2012399f43ad7e45f415ec
hong-sile <hyukjoon.dev@gmail.com>
2024-08-04 17:30:03 +0900

    refactor: thumbnail urls 분리(#46)

---
+++ src/main/java/play/pluv/oauth/google/dto/ThumbnailUrls.java
@@ -0,0 +1,17 @@
+package play.pluv.oauth.google.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+record ThumbnailUrls(
+    @JsonProperty("default")
+    Thumbnail thumbnail
+) {
+
+  public String getUrl() {
+    return thumbnail.url;
+  }
+
+  private record Thumbnail(String url) {
+
+  }
+}

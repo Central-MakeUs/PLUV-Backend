4a37de37fea6fdde22d1687aae80a1bc2461fb0c
hong-sile <hyukjoon.dev@gmail.com>
2024-08-05 16:20:22 +0900

    feat: 웹 요청/응답을 로깅하는 기능 추가(#47)

---
+++ src/main/java/play/pluv/config/LogFilter.java
@@ -0,0 +1,58 @@
+package play.pluv.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Slf4j
+public class LogFilter implements Filter {
+
+  @Override
+  public void doFilter(
+      final ServletRequest request, final ServletResponse response, final FilterChain filterChain
+  ) throws IOException, ServletException {
+    final ContentCachingRequestWrapper httpServletRequest
+        = new ContentCachingRequestWrapper((HttpServletRequest) request);
+    final ContentCachingResponseWrapper httpServletResponse
+        = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+    loggingRequest(httpServletRequest);
+    filterChain.doFilter(httpServletRequest, httpServletResponse);
+    loggingResponse(httpServletResponse);
+  }
+
+  private static void loggingRequest(final ContentCachingRequestWrapper httpServletRequest) {
+    final String uri = httpServletRequest.getRequestURI();
+
+    //request 내용 확인
+    final String reqContent = new String(httpServletRequest.getContentAsByteArray());
+    MDC.put("uri", uri);
+    MDC.put("contentBody", reqContent);
+    log.info("request logging");
+    MDC.clear();
+  }
+
+  private static void loggingResponse(
+      final ContentCachingResponseWrapper httpServletResponse
+  ) throws IOException {
+    int httpStatus = httpServletResponse.getStatus();
+    final String resContent = new String(httpServletResponse.getContentAsByteArray());
+
+    //주의 : response를 클라이언트에서 볼 수 있도록 하려면 response를 복사해야 한다. response를 콘솔에 보여주면 내용이 사라진다.
+    httpServletResponse.copyBodyToResponse();
+
+    MDC.put("httpStatus", String.valueOf(httpStatus));
+    MDC.put("contentBody", resContent);
+    log.info("response logging");
+    MDC.clear();
+  }
+}

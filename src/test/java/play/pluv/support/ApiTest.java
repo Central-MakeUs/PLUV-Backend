c467d86890d267165fe103423bdb8d516f55b7a9
hong-sile <hyukjoon.dev@gmail.com>
2024-08-08 18:30:29 +0900

    test: MvcTest 옵션 수정(#51)

--- src/test/java/play/pluv/support/ApiTest.java
+++ src/test/java/play/pluv/support/ApiTest.java
@@ -4,30 +4,38 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import play.pluv.base.LocalDateTimeProvider;
 import play.pluv.login.application.JwtProvider;
 import play.pluv.login.application.LoginService;
 import play.pluv.music.application.MusicService;
 import play.pluv.oauth.application.OAuthService;
 import play.pluv.playlist.application.PlayListService;
+import play.pluv.support.ApiTest.RestDocsResultConfig;
 
 @WebMvcTest
-@Import(JwtProvider.class)
+@Import({JwtProvider.class, RestDocsResultConfig.class})
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@MockBean(JpaMetamodelMappingContext.class)
 public abstract class ApiTest {
 
   @Autowired
@@ -53,5 +61,14 @@ public abstract class ApiTest {
             .withRequestDefaults(prettyPrint())
             .withResponseDefaults(prettyPrint()))
         .build();
+  }
+
+  @Configuration
+  static class RestDocsResultConfig {
+
+    @Bean
+    public LocalDateTimeProvider localDateTimeProvider() {
+      return new LocalDateTimeProvider();
+    }
   }
 }

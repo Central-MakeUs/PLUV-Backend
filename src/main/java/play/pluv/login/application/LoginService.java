8fba9f98a1a8a6f719e6939d043363668ee329f6
hong-sile <hyukjoon.dev@gmail.com>
2024-08-10 15:23:22 +0900

    feat: Compoisite에 있는 로그인 방법 추가(#83)

--- src/main/java/play/pluv/login/application/LoginService.java
+++ src/main/java/play/pluv/login/application/LoginService.java
@@ -2,6 +2,7 @@ package play.pluv.login.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import play.pluv.member.domain.Member;
 import play.pluv.oauth.application.SocialLoginClientComposite;
 import play.pluv.oauth.domain.OAuthMemberInfo;
@@ -15,6 +16,7 @@ public class LoginService {
   private final RegisterUpdater registerUpdater;
   private final SocialLoginClientComposite socialLoginClientComposite;
 
+  @Transactional
   public Long createToken(final MusicStreaming serverType, final String key) {
     final OAuthMemberInfo memberInfo = socialLoginClientComposite
         .fetchMemberInfo(serverType, key);
@@ -23,5 +25,15 @@ public class LoginService {
         .orElseGet(() -> registerUpdater.registerNewMember(memberInfo));
 
     return member.getId();
+  }
+
+  @Transactional
+  public void addOtherLoginWay(
+      final MusicStreaming serverType, final Long memberId, final String key
+  ) {
+    final OAuthMemberInfo memberInfo = socialLoginClientComposite
+        .fetchMemberInfo(serverType, key);
+
+    registerUpdater.addOtherLoginSource(memberId, memberInfo);
   }
 }

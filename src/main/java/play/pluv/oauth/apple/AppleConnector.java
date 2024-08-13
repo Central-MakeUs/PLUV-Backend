8e577e4bf233614c067549dd411d6cec63596309
hong-sile <hyukjoon.dev@gmail.com>
2024-08-14 01:30:37 +0900

    feat: apple accessToken, idToken 발급 로직 추가(#41)

--- src/main/java/play/pluv/oauth/apple/AppleConnector.java
+++ src/main/java/play/pluv/oauth/apple/AppleConnector.java
@@ -1,19 +1,25 @@
 package play.pluv.oauth.apple;
 
-import static io.jsonwebtoken.io.Decoders.BASE64;
 import static java.lang.System.currentTimeMillis;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static play.pluv.login.exception.LoginExceptionType.GENERATE_APPLE_CLIENT_SECRET_ERROR;
 import static play.pluv.playlist.domain.MusicStreaming.APPLE;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import play.pluv.login.exception.LoginException;
 import play.pluv.oauth.apple.dto.AppleTokenResponse;
 import play.pluv.oauth.application.SocialLoginClient;
 import play.pluv.oauth.domain.OAuthMemberInfo;
@@ -31,12 +37,11 @@ public class AppleConnector implements SocialLoginClient {
 
   @Override
   public OAuthMemberInfo fetchMember(final String authCode) {
-    final MultiValueMap<String, String> param = createRequestParamForAccessToken(authCode);
-    final AppleTokenResponse appleTokenResponse = appleApiClient.fetchToken(param);
+    final AppleTokenResponse appleToken = geTokens(authCode);
     return null;
   }
 
-  public AppleTokenResponse fetchMemberTest(final String authCode) {
+  public AppleTokenResponse geTokens(final String authCode) {
     final MultiValueMap<String, String> param = createRequestParamForAccessToken(authCode);
     return appleApiClient.fetchToken(param);
   }
@@ -56,25 +61,6 @@ public class AppleConnector implements SocialLoginClient {
     return param;
   }
 
-  //  @Override
-//  public OAuth2MemberResponse getMemberResponse(String authCode) {
-//    AppleTokenResponse tokenResponse;
-//    try {
-//      tokenResponse = appleOAuth2Client.getToken(
-//          appleOAuth2Properties.getClientId(),
-//          generateClientSecret(),
-//          GRANT_TYPE,
-//          authCode
-//      );
-//    } catch (Exception e) {
-//      throw new BaseException(AppleExceptionType.TOKEN_ERROR, e);
-//    }
-//    String idToken = tokenResponse.getIdToken();
-//    AppleTokenIdPayload appleTokenIdPayload = jwtProvider.decodePayload(idToken,
-//        AppleTokenIdPayload.class);
-//    return new AppleOAuth2MemberResponse(appleTokenIdPayload.getEmail());
-//  }
-//
   private String generateClientSecret() {
     final Map<String, Object> jwtHeaders = Map.of(
         "kid", appleConfigProperty.keyId(),
@@ -88,20 +74,18 @@ public class AppleConnector implements SocialLoginClient {
         .audience().add(AUDIENCE).and()
         .expiration(new Date(EXP + currentTimeMillis()))
         .subject(appleConfigProperty.clientId())
-        .signWith(Keys.hmacShaKeyFor(BASE64.decode(appleConfigProperty.privateKey())))
+        .signWith(getPrivateKeyFromPem(appleConfigProperty.privateKey()))
         .compact();
   }
 
-//  private PrivateKey generatePrivateKey() {
-//    Security.addProvider(new BouncyCastleProvider());
-//    JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-//
-//    try {
-//      byte[] privateKeyBytes = Base64.getDecoder().decode(appleOAuth2Properties.getPrivateKey());
-//
-//      return converter.getPrivateKey(PrivateKeyInfo.getInstance(privateKeyBytes));
-//    } catch (Exception e) {
-//      throw new RuntimeException("애플에 전송시 사용할 키 생성 중 예외가 발생했습니다.", e);
-//    }
-//  }
+  private static ECPrivateKey getPrivateKeyFromPem(final String privateKey) {
+    try {
+      final byte[] decoded = Base64.getDecoder().decode(privateKey);
+      final PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+      final KeyFactory keyFactory = KeyFactory.getInstance("EC");
+      return (ECPrivateKey) keyFactory.generatePrivate(keySpec);
+    } catch (final NoSuchAlgorithmException | InvalidKeySpecException e) {
+      throw new LoginException(GENERATE_APPLE_CLIENT_SECRET_ERROR);
+    }
+  }
 }

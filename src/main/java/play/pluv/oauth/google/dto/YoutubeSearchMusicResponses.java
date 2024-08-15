999cc20b767331f746ef75e31d406b5c2190a9fc
hong-sile <hyukjoon.dev@gmail.com>
2024-08-15 17:01:18 +0900

    refactor: 미사용 로직 삭제(#88)

--- src/main/java/play/pluv/oauth/google/dto/YoutubeSearchMusicResponses.java
+++ src/main/java/play/pluv/oauth/google/dto/YoutubeSearchMusicResponses.java
@@ -3,7 +3,6 @@ package play.pluv.oauth.google.dto;
 import static play.pluv.playlist.domain.MusicStreaming.YOUTUBE;
 
 import java.util.List;
-import java.util.Optional;
 import play.pluv.music.domain.DestinationMusic;
 import play.pluv.music.domain.DestinationMusics;
 import play.pluv.music.domain.MusicId;
@@ -11,22 +10,6 @@ import play.pluv.music.domain.MusicId;
 public record YoutubeSearchMusicResponses(
     List<YoutubeMusicVideo> items
 ) {
-
-  public Optional<DestinationMusic> toDestinationMusic() {
-    if (items.isEmpty()) {
-      return Optional.empty();
-    }
-    final YoutubeMusicVideo youtubeMusicVideo = items.get(0);
-
-    return Optional.of(DestinationMusic.builder()
-        .musicId(new MusicId(YOUTUBE, youtubeMusicVideo.getId()))
-        .imageUrl(youtubeMusicVideo.snippet().thumbnails().getUrl())
-        .artistNames(List.of())
-        .title(youtubeMusicVideo.snippet().title())
-        .isrcCode(null)
-        .build()
-    );
-  }
 
   public DestinationMusics toDestinationMusics() {
     return new DestinationMusics(items.stream()

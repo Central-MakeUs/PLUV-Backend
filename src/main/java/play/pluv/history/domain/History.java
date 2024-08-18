af3942e6757a388b1b9a69461421e28ede9dc8c8
hong-sile <hyukjoon.dev@gmail.com>
2024-08-18 17:47:13 +0900

    feat: History를 memberId로 조회하는 기능 추가(#85)

--- src/main/java/play/pluv/history/domain/History.java
+++ src/main/java/play/pluv/history/domain/History.java
@@ -4,12 +4,15 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import play.pluv.base.BaseEntity;
+import play.pluv.playlist.domain.MusicStreaming;
 
 @Entity
 @NoArgsConstructor(access = PROTECTED)
@@ -19,26 +22,26 @@ public class History extends BaseEntity {
   @Id
   @GeneratedValue(strategy = IDENTITY)
   private Long id;
-
   private String title;
-
   private String thumbNailUrl;
-
-  private int transferredSongCount;
-
-  private int totalSongCount;
-
+  private Integer transferredSongCount;
+  private Integer totalSongCount;
   private Long memberId;
+  @Enumerated(EnumType.STRING)
+  private MusicStreaming source;
+  @Enumerated(EnumType.STRING)
+  private MusicStreaming destination;
 
   @Builder
-  public History(
-      final String title, final String thumbNailUrl, final int transferredSongCount,
-      final int totalSongCount, final Long memberId
-  ) {
+  public History(final String title, final String thumbNailUrl, final Integer transferredSongCount,
+      final Integer totalSongCount, final Long memberId, final MusicStreaming source,
+      final MusicStreaming destination) {
     this.title = title;
     this.thumbNailUrl = thumbNailUrl;
     this.transferredSongCount = transferredSongCount;
     this.totalSongCount = totalSongCount;
     this.memberId = memberId;
+    this.source = source;
+    this.destination = destination;
   }
 }
--- src/main/java/play/pluv/history/domain/repository/HistoryRepository.java
+++ src/main/java/play/pluv/history/domain/repository/HistoryRepository.java
@@ -1,8 +1,17 @@
 package play.pluv.history.domain.repository;
 
+import static play.pluv.history.exception.HistoryExceptionType.HISTORY_NOT_FOUND;
+
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import play.pluv.history.domain.History;
+import play.pluv.history.exception.HistoryException;
 
 public interface HistoryRepository extends JpaRepository<History, Long> {
 
+  Optional<History> findByMemberId(final Long memberId);
+
+  default History readByMemberId(final Long memberId) {
+    return findByMemberId(memberId).orElseThrow(() -> new HistoryException(HISTORY_NOT_FOUND));
+  }
 }

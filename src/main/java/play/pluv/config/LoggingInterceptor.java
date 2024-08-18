960b1a9a9b2ca0ca4f62660214b8025fdcba5946
hong-sile <hyukjoon.dev@gmail.com>
2024-08-19 00:49:31 +0900

    feat: Local, dev 환경에서만 로깅하도록 수정

--- src/main/java/play/pluv/config/LoggingInterceptor.java
+++ src/main/java/play/pluv/config/LoggingInterceptor.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -28,39 +27,32 @@ public class LoggingInterceptor implements ClientHttpRequestInterceptor {
   }
 
   private void logging(final HttpRequest request, final byte[] body) {
-    MDC.put("method", request.getMethod().toString());
-    MDC.put("uri", request.getURI().toString());
-    loggingHeader(request);
-    loggingBody(body);
-    log.info("외부 api 요청");
-    MDC.clear();
-  }
-
-  private static void loggingBody(final byte[] body) {
     final StringBuilder sb = new StringBuilder();
+
+    sb.append(String.format("%s %s \n", request.getMethod(), request.getURI()));
+
+    request.getHeaders().forEach(
+        (header, values) -> sb.append(String.format("%s : %s \n", header, values))
+    );
+
     if (body.length > 0) {
       sb.append("\n");
       sb.append(new String(body));
     }
-    MDC.put("body", sb.toString());
-  }
 
-  private static void loggingHeader(final HttpRequest request) {
-    final StringBuilder sb = new StringBuilder();
-    request.getHeaders().forEach(
-        (header, values) -> sb.append(String.format("%s : %s \n", header, values))
-    );
-    MDC.put("header", sb.toString());
+    log.info("Request : \n{}", sb);
   }
 
   private void logging(final ClientHttpResponse response) throws IOException {
-    final InputStreamReader isr = new InputStreamReader(response.getBody(), StandardCharsets.UTF_8);
+    final StringBuilder sb = new StringBuilder();
+    final InputStreamReader isr = new InputStreamReader(
+        response.getBody(), StandardCharsets.UTF_8);
     final String body = new BufferedReader(isr).lines()
         .collect(Collectors.joining("\n"));
 
-    MDC.put("status", response.getStatusCode().toString());
-    MDC.put("body", body);
-    log.info("외부 api 응답");
-    MDC.clear();
+    sb.append(String.format("%s\n", response.getStatusCode()));
+    sb.append("\n");
+    sb.append(body);
+    log.info("Response : \n{}", sb);
   }
 }

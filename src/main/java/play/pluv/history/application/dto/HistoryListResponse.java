8efdfc6c5b46993ed6503af7f13307cfe9136632
hong-sile <hyukjoon.dev@gmail.com>
2024-08-22 16:24:10 +0900

    refactor: 이전된 음악수로 변경(#101)

--- src/main/java/play/pluv/history/application/dto/HistoryListResponse.java
+++ src/main/java/play/pluv/history/application/dto/HistoryListResponse.java
@@ -1,12 +1,14 @@
 package play.pluv.history.application.dto;
 
+import static java.util.Comparator.comparing;
+
 import java.time.LocalDate;
 import java.util.List;
 import play.pluv.history.domain.History;
 
 public record HistoryListResponse(
     Long id,
-    Integer totalSongCount,
+    Integer transferredSongCount,
     LocalDate transferredDate,
     String title,
     String imageUrl
@@ -15,12 +17,13 @@ public record HistoryListResponse(
   public static List<HistoryListResponse> createList(final List<History> histories) {
     return histories.stream()
         .map(HistoryListResponse::from)
+        .sorted(comparing(HistoryListResponse::transferredDate).reversed())
         .toList();
   }
 
   private static HistoryListResponse from(final History history) {
     return new HistoryListResponse(
-        history.getId(), history.getTotalSongCount(), history.getCreatedAt().toLocalDate(),
+        history.getId(), history.getTransferredSongCount(), history.getCreatedAt().toLocalDate(),
         history.getTitle(), history.getThumbNailUrl()
     );
   }
--- src/test/java/play/pluv/api/HistoryApiTest.java
+++ src/test/java/play/pluv/api/HistoryApiTest.java
@@ -12,11 +12,13 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.pr
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
-import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static play.pluv.fixture.HistoryEntityFixture.히스토리_1;
 import static play.pluv.fixture.HistoryEntityFixture.히스토리들;
 
 import org.junit.jupiter.api.Test;
@@ -47,10 +49,45 @@ public class HistoryApiTest extends ApiTest {
                 fieldWithPath("msg").type(STRING).description("상태 코드에 해당하는 메시지"),
                 fieldWithPath("data[]").type(ARRAY).description("히스토리 목록"),
                 fieldWithPath("data[].id").type(NUMBER).description("히스토리의 Id"),
-                fieldWithPath("data[].totalSongCount").type(NUMBER).description("이전한 곡수"),
+                fieldWithPath("data[].transferredSongCount").type(NUMBER).description("이전한 곡수"),
                 fieldWithPath("data[].title").type(STRING).description("이전한 음악 타이틀"),
                 fieldWithPath("data[].imageUrl").type(STRING).description("이미지 url"),
                 fieldWithPath("data[].transferredDate").type(STRING).description("이전된 날짜")
+            )
+        ));
+  }
+
+  @Test
+  void 히스토리_상세조회한다() throws Exception {
+    final Long memberId = 10L;
+    final Long historyId = 3L;
+    final String token = "access Token";
+    final var history = 히스토리_1(memberId);
+
+    setAccessToken(token, memberId);
+    when(historyService.findHistory(historyId)).thenReturn(history);
+
+    mockMvc.perform(get("/history/{id}", historyId)
+            .header(AUTHORIZATION, "Bearer " + token))
+        .andExpect(status().isOk())
+        .andDo(document("history-detail",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            pathParameters(
+                parameterWithName("id").description("히스토리의 식별자")
+            ),
+            requestHeaders(
+                headerWithName(AUTHORIZATION).description("Bearer Token")
+            ),
+            responseFields(
+                fieldWithPath("code").type(NUMBER).description("상태 코드"),
+                fieldWithPath("msg").type(STRING).description("상태 코드에 해당하는 메시지"),
+                fieldWithPath("data.id").type(NUMBER).description("히스토리의 Id"),
+                fieldWithPath("data.totalSongCount").type(NUMBER).description("이전하려 했던 곡수"),
+                fieldWithPath("data.title").type(STRING).description("이전한 음악 타이틀"),
+                fieldWithPath("data.imageUrl").type(STRING).description("이미지 url"),
+                fieldWithPath("data.source").type(STRING).description("이전하려했던 플레이리스트 출처"),
+                fieldWithPath("data.destination").type(STRING).description("이전한 플레이리스트 목적지")
             )
         ));
   }

6e01be04237d693685a54bd58180640a0dce3b6f
hong-sile <hyukjoon.dev@gmail.com>
2024-08-23 01:43:20 +0900

    feat: 피드를 북마크하는 기능 추가(#105)

--- src/test/java/play/pluv/feed/application/FeedServiceTest.java
+++ src/test/java/play/pluv/feed/application/FeedServiceTest.java
@@ -3,12 +3,16 @@ package play.pluv.feed.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static play.pluv.fixture.FeedFixture.저장된_피드_1;
 import static play.pluv.fixture.FeedFixture.저장된_피드_2;
+import static play.pluv.fixture.MemberEntityFixture.멤버_홍혁준;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import play.pluv.feed.domain.Feed;
 import play.pluv.feed.domain.repository.FeedRepository;
+import play.pluv.fixture.MemberEntityFixture;
+import play.pluv.member.domain.Member;
+import play.pluv.member.domain.repository.MemberRepository;
 import play.pluv.support.ApplicationTest;
 
 class FeedServiceTest extends ApplicationTest {
@@ -17,6 +21,10 @@ class FeedServiceTest extends ApplicationTest {
   private FeedService feedService;
   @Autowired
   private FeedRepository feedRepository;
+  @Autowired
+  private MemberRepository memberRepository;
+  @Autowired
+  private FeedReader feedReader;
 
   @Test
   void 피드목록을_조회한다() {
@@ -28,5 +36,20 @@ class FeedServiceTest extends ApplicationTest {
     assertThat(actual)
         .usingRecursiveFieldByFieldElementComparator()
         .containsExactlyInAnyOrder(savedFeed1, savedFeed2);
+  }
+
+  @Test
+  void 피드를_북마크한다() {
+    final Member member = 멤버_홍혁준(memberRepository);
+    final Feed savedFeed1 = 저장된_피드_1(feedRepository);
+    저장된_피드_2(feedRepository);
+
+    feedService.bookmarkFeed(member.getId(), savedFeed1.getId());
+
+    final List<Feed> actual = feedReader.findBookmarkedFeeds(member.getId());
+
+    assertThat(actual)
+        .usingRecursiveFieldByFieldElementComparator()
+        .containsExactlyInAnyOrder(savedFeed1);
   }
 }

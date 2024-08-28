9564d3fb3b77b6447c992bdf08775b1d312c2cc6
hong-sile <hyukjoon.dev@gmail.com>
2024-08-29 00:01:21 +0900

    feat: feed 북마크 취소 기능 api 변경(#127)

--- src/main/java/play/pluv/feed/controller/FeedController.java
+++ src/main/java/play/pluv/feed/controller/FeedController.java
@@ -2,10 +2,13 @@ package play.pluv.feed.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import play.pluv.base.BaseResponse;
 import play.pluv.feed.application.FeedService;
@@ -43,7 +46,7 @@ public class FeedController {
     return BaseResponse.ok("");
   }
 
-  @PostMapping("/{id}/cancel")
+  @DeleteMapping("/{id}/save")
   public BaseResponse<String> cancelBookmarkFeed(
       final JwtMemberId jwtMemberId, @PathVariable final Long id
   ) {

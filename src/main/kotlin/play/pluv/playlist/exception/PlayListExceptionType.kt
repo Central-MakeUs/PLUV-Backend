310b0bfd0c225ad8b542a7d0f7a87d2675d5d2d3
hong-sile <hyukjoon.dev@gmail.com>
2025-07-23 14:54:44 +0900

    refactor: playlist exception refactor

---
+++ src/main/kotlin/play/pluv/playlist/exception/PlayListExceptionType.kt
@@ -0,0 +1,15 @@
+package play.pluv.playlist.exception
+
+import lombok.Getter
+import lombok.RequiredArgsConstructor
+import org.springframework.http.HttpStatus
+import play.pluv.base.BaseExceptionType
+
+enum class PlayListExceptionType(
+    override val httpStatus: HttpStatus,
+    override val message: String
+) : BaseExceptionType {
+
+    PLAYLIST_PROVIDER_NOT_FOUND(HttpStatus.NOT_FOUND, "지원하지 않는 스트리밍 서비스입니다");
+
+}
